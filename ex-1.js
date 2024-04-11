72ff0ee813c0664d21977c3cb03c111bf2f2b1e1
Boom <boom2k31@gmail.com>
2024-04-11 16:48:15 +0700

    Finish Assign

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,20 @@
 
 function forEach(array, operation) {
   // Start coding here
+  let n = [];
+    for (let i=0 ; i<array.length ; i++){
+      n.push(add5K(array[i]));
+    } return n;
+}
+function add5K (num){
+  return num + 5000 ;
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
+let newEmployeeSalaries = [];
 
 // Using `forEach` function here
+
+newEmployeeSalaries = forEach(employeeSalaries,add5K);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
